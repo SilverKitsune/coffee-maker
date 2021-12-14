a0ae8969d473940bdd59201267fa610ffc5f39e7
Alla <zhalla1999@mail.ru>
2021-12-14 22:24:04 +0300

    Добавлена сущность Action

--- coffee-maker/src/main/java/my/aisa/test/models/Action.java
+++ coffee-maker/src/main/java/my/aisa/test/models/Action.java
@@ -1,8 +1,25 @@
 package my.aisa.test.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "actions")
 public class Action {
+    @Id
+    @Column(name = "date")
     private String dateAndTime;
+
+    @Column(name = "message")
     private String actionMessage;
+
+    public Action() {
+        dateAndTime = new Date().toString();
+        actionMessage = "Ничего не произошло";
+    }
 
     public Action(String dateAndTime, String actionMessage) {
         this.actionMessage = actionMessage;
